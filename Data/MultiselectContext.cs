0305b97a66adc5152595892cf2da305920732dbe
Andy Angelov <47538977+Catman1000@users.noreply.github.com>
2021-07-27 20:35:34 +0300

    Added more names

--- Data/MultiselectContext.cs
+++ Data/MultiselectContext.cs
@@ -24,13 +24,25 @@ namespace multiselect.Data
             modelBuilder.Entity<UserModel>()
                 .HasData(
             new UserModel { ID = 1, FullName = "Carson Li", Selected = false, SelectedDate = DateTime.Parse("2005-09-01") },
-            new UserModel { ID = 2, FullName = "Meredith Ivanova", Selected = false, SelectedDate = DateTime.Parse("2002-09-01") },
+            new UserModel { ID = 2, FullName = "Meredith Backpfeifengesicht", Selected = false, SelectedDate = DateTime.Parse("2002-09-01") },
             new UserModel { ID = 3, FullName = "Arturo Jopalov", Selected = false, SelectedDate = DateTime.Parse("2003-09-01") },
-            new UserModel { ID = 4, FullName = "Gytis Peskha", Selected = false, SelectedDate = DateTime.Parse("2002-09-01") },
+            new UserModel { ID = 4, FullName = "Gytis Peshka", Selected = false, SelectedDate = DateTime.Parse("2002-09-01") },
             new UserModel { ID = 5, FullName = "Yan Li", Selected = false, SelectedDate = DateTime.Parse("2002-09-01") },
             new UserModel { ID = 6, FullName = "Peggy Poo", Selected = false, SelectedDate = DateTime.Parse("2001-09-01") },
             new UserModel { ID = 7, FullName = "Laura Dee", Selected = false, SelectedDate = DateTime.Parse("2003-09-01") },
-            new UserModel { ID = 8, FullName = "Nina Nani", Selected = false, SelectedDate = DateTime.Parse("2005-09-01") }
+            new UserModel { ID = 8, FullName = "Nina Nani", Selected = false, SelectedDate = DateTime.Parse("2005-09-01") },
+            new UserModel { ID = 9, FullName = "Ivan Ivanov", Selected = false, SelectedDate = DateTime.Parse("2005-09-01") },
+            new UserModel { ID = 10, FullName = "Zhang Wei", Selected = false, SelectedDate = DateTime.Parse("2002-09-01") },
+            new UserModel { ID = 11, FullName = "Lei Ying Lo", Selected = false, SelectedDate = DateTime.Parse("2003-09-01") },
+            new UserModel { ID = 12, FullName = "Yu Stin Ki Pu", Selected = false, SelectedDate = DateTime.Parse("2002-09-01") },
+            new UserModel { ID = 13, FullName = "Ai Bang Mai Ni", Selected = false, SelectedDate = DateTime.Parse("2002-09-01") },
+            new UserModel { ID = 14, FullName = "Wai Fu Pi", Selected = false, SelectedDate = DateTime.Parse("2001-09-01") },
+            new UserModel { ID = 15, FullName = "Armpid Stiki", Selected = false, SelectedDate = DateTime.Parse("2003-09-01") },
+            new UserModel { ID = 16, FullName = "Madam Uke Kabore", Selected = false, SelectedDate = DateTime.Parse("2005-09-01")},
+            new UserModel { ID = 17, FullName = "Hank Huley", Selected = false, SelectedDate = DateTime.Parse("2003-09-01") },
+            new UserModel { ID = 18, FullName = "Jack Goff", Selected = false, SelectedDate = DateTime.Parse("2005-09-01") },
+            new UserModel { ID = 19, FullName = "Herculaño Hepres", Selected = false, SelectedDate = DateTime.Parse("2005-09-01") },
+            new UserModel { ID = 20, FullName = "Ingannamorte Collates", Selected = false, SelectedDate = DateTime.Parse("2002-09-01") }
     );
         }
     }
---
+++ Migrations/20210727171939_More-Names.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace multiselect.Migrations
+{
+    public partial class MoreNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "FullName",
+                value: "Meredith Backpfeifengesicht");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "ID",
+                keyValue: 4,
+                column: "FullName",
+                value: "Gytis Peshka");
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "ID", "FullName", "Selected", "SelectedDate" },
+                values: new object[,]
+                {
+                    { 9, "Ivan Ivanov", false, new DateTime(2005, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 10, "Zhang Wei", false, new DateTime(2002, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 11, "Lei Ying Lo", false, new DateTime(2003, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 12, "Yu Stin Ki Pu", false, new DateTime(2002, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 13, "Ai Bang Mai Ni", false, new DateTime(2002, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 14, "Wai Fu Pi", false, new DateTime(2001, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 15, "Armpid Stiki", false, new DateTime(2003, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 16, "Madam Uke Kabore", false, new DateTime(2005, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 17, "Hank Huley", false, new DateTime(2003, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 18, "Jack Goff", false, new DateTime(2005, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 19, "Herculaño Hepres", false, new DateTime(2005, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 20, "Ingannamorte Collates", false, new DateTime(2002, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "ID",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "ID",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "ID",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "ID",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "ID",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "ID",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "ID",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "ID",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "ID",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "ID",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "ID",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "ID",
+                keyValue: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "FullName",
+                value: "Meredith Ivanova");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "ID",
+                keyValue: 4,
+                column: "FullName",
+                value: "Gytis Peskha");
+        }
+    }
+}
